13d52c4407826425047abecbd331ff64a62be601
Sercan PASPAL <221703504@ogr.uludag.edu.tr>
2021-05-07 13:01:23 +0300

    repository address add to corner

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,10 +1,14 @@
+import GithubCorner from "../github-corner";
 import styles from "./index.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.content}>
-      <h1 className={styles.title}>Netflix Listeleri</h1>
-      <div>{children}</div>
+    <div>
+      <GithubCorner />
+      <div className={styles.content}>
+        <h1 className={styles.title}>Netflix Listeleri</h1>
+        <div>{children}</div>
+      </div>
     </div>
   );
 };
