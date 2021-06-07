061a5fdea98b2c085e4a2e5e5ee0632d77f4fd0d
Sercan PASPAL <221703504@ogr.uludag.edu.tr>
2021-06-07 10:36:00 +0300

    localization words

--- components/genre-list/index.js
+++ components/genre-list/index.js
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation";
 import { useEffect } from "react";
 import { usePagination } from "../../hooks";
 import Button from "../button";
@@ -5,6 +6,7 @@ import GenreListItem from "../genre-list-item";
 import styles from "./index.module.css";
 
 const GenreList = ({ genres }) => {
+  const { t } = useTranslation("common");
   const [data, page, setPage, hasMore] = usePagination(genres);
 
   useEffect(() => {
@@ -13,7 +15,9 @@ const GenreList = ({ genres }) => {
 
   return (
     <div>
-      <h2 className={styles.title}>Tür Listesi ({genres.length})</h2>
+      <h2 className={styles.title}>
+        {t("genre_list")} ({genres.length})
+      </h2>
       <ul className={styles.list}>
         {data.map((genre, _i) => (
           <GenreListItem genre={genre} key={_i} />
@@ -21,7 +25,7 @@ const GenreList = ({ genres }) => {
       </ul>
       <div className="text-center p-5">
         {hasMore && (
-          <Button onClick={() => setPage(page + 1)}>Daha fazla yükle</Button>
+          <Button onClick={() => setPage(page + 1)}>{t("load_more")}</Button>
         )}
       </div>
     </div>
--- components/tag-list/index.js
+++ components/tag-list/index.js
@@ -1,10 +1,15 @@
+import useTranslation from "next-translate/useTranslation";
 import TagListItem from "../tag-list-item";
 import styles from "./index.module.css";
 
 const TagList = ({ tags }) => {
+  const { t } = useTranslation("common");
+
   return (
     <div>
-      <h2 className={styles.title}>Etikete Göre Filtrele ({tags.length})</h2>
+      <h2 className={styles.title}>
+        {t("filter_by_tag")} ({tags.filter((t) => !t.disabled).length})
+      </h2>
       <ul className={styles.list}>
         {tags.map((tag, _i) => (
           <TagListItem tag={tag} key={_i} />
