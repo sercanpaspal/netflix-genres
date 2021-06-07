f593a440394791059e69a9376768b189e9ec0bbe
Sercan PASPAL <221703504@ogr.uludag.edu.tr>
2021-06-07 10:35:47 +0300

    select language ui

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,21 @@
+import useTranslation from "next-translate/useTranslation";
 import GithubCorner from "../github-corner";
+import LanguageMenu from "../language-menu";
 import styles from "./index.module.css";
 
 const Layout = ({ children }) => {
+  const { t } = useTranslation("common");
+
   return (
     <div>
       <GithubCorner />
       <div className={styles.content}>
-        <h1 className={styles.title}>Netflix Listeleri</h1>
+        <div className={styles.header}>
+          <h1 className={styles.title}>{t("title")}</h1>
+          <div>
+            <LanguageMenu />
+          </div>
+        </div>
         <div>{children}</div>
       </div>
     </div>
